8a5e0569b372b2ecaa70c343037c2448a526b804
dana26kw <dana1a2b3c@gmail.com>
2022-03-15 14:44:14 +0300

    made my  first game

---
+++ c3-cw3/c3-cw3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace c3_cw3
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            void OnMousedrag() {
+                Vector2 mousepos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                Vector2 playerpos = Camera.main.screenToWorldPoint(mousepos);
+                transform.position = playerPos; }
+
+            void OnCollisionEnter2D(Collision2D other)
+            {
+                SceneManager.LoadScene("Lvl1");
+            }
+
+            void OnTriggerEnter2D(Collider2D other) {
+
+                SceneManager.LoadScene("Lvl2");
+            }
+            void start()
+            {
+
+            }
+
+            void update()
+            {
+
+            }
+
+        }
+    }
+}
+
